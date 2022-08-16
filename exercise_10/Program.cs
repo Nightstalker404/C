763dc80f5d452cfa0b08f92bf8b3a045795ed822
Vegzuz <vegzus@gmail.com>
2022-08-16 19:25:18 +0300

    урок 2. домашнее задание

---
+++ exercise_10/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+Console.Write("Введите число от 100 до 999: ");
+int number = int.Parse(Console.ReadLine());
+if (number > 99 & number < 1000)
+{
+    Console.WriteLine(number / 10 % 10);
+}
+else
+{
+    Console.WriteLine("Введено некорректное число");
+}
+
---
+++ exercise_15/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру,
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+Console.Write("Введите число дня недели: ");
+int number = int.Parse(Console.ReadLine());
+if (number == 6 ^ number == 7)
+{
+    Console.WriteLine("да");
+}
+else if (number < 6 & number > 0)
+{
+    Console.WriteLine("нет");
+}
+else 
+{
+    Console.WriteLine("Введено некорректное число. Введите число от 1 до 7");
+}
