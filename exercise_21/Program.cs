df4b383859fa9d1d6568eb489f56b487606f7c75
Vegzuz <vegzus@gmail.com>
2022-08-19 02:02:24 +0300

    Урок 3. домашние задания

---
+++ exercise_21/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 21 Напишите программу, которая принимает на вход координаты двух точек 
+// и находит расстояние между ними в 3D пространстве.
+Console.Write("Введите координаты точки А: ");
+int Ax = int.Parse(Console.ReadLine());
+int Ay = int.Parse(Console.ReadLine());
+int Az = int.Parse(Console.ReadLine());
+Console.Write("Введите координаты точки B: ");
+int Bx = int.Parse(Console.ReadLine());
+int By = int.Parse(Console.ReadLine());
+int Bz = int.Parse(Console.ReadLine());
+double distance = Math.Sqrt(Math.Pow(Ax - Bx, 2) + Math.Pow(Ay - By, 2) + Math.Pow(Az - Bz, 2));
+Console.Write($"Расстояние между равно: {Math.Round(distance, 2)}");
