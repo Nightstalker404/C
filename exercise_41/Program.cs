38f5438e00eaa9312db3f263dbb076c813ceaf81
Vegzuz <vegzus@gmail.com>
2022-08-30 19:16:29 +0300

    Урок 6. Домашние задания

---
+++ exercise_41/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+int[] Arr(int num)
+{
+    int[] arr = new int[num];
+    for (int i = 0; i < num; i++)
+    {
+        Console.WriteLine("Введите число:");
+        arr[i] = int.Parse(Console.ReadLine());
+    }
+    return arr;
+}
+
+int PositiveSum(int[] mas)
+{
+    int count = 0;
+    for (int i = 0; i < mas.Length; i++) 
+    {
+        if (mas[i] > 0)
+            count += 1;
+
+    }
+    return count;
+}
+
+Console.WriteLine("Введите количество символов массива: ");
+int num = int.Parse(Console.ReadLine());
+int[] arr = Arr(num);
+Console.WriteLine($"[{String.Join(" ", arr)}]");
+Console.WriteLine($"Количество положительных чисел массива: {PositiveSum(arr)}");
---
+++ exercise_43/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+void Point(double k1, double k2, double b1, double b2)
+{
+    double y1 = 0;
+    double y2 = 0;
+    double x = 0;
+    if (k1 != k2)
+    {
+        x = (b2 - b1) / (k1 -k2);
+        y1 = k1 * x + b1;
+        y2 = k2 * x + b2;
+    }
+    else
+    {
+        Console.WriteLine("Точек пересечения нет");
+    }
+    Console.WriteLine($"({y1}; {y2})");
+}
+Console.WriteLine("Введите точку к1: ");
+double k1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите точку к2: ");
+double k2 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите точку b1: ");
+double b1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите точку b2: ");
+double b2 = double.Parse(Console.ReadLine());
+Point(k1, k2, b1, b2);
