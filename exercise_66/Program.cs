a69899867d9841fde2d8fd9b9bc923fce1c11893
Vegzuz <vegzus@gmail.com>
2022-09-08 21:28:23 +0300

    Урок 9. Домашние задания

---
+++ exercise_66/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 66: Задайте значения M и N. 
+// Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+
+int SumElements(int m, int n)
+{
+    if ( n <= m)
+        return n;
+    return n + SumElements(m, n - 1);
+}
+
+
+Console.WriteLine("Введите m: ");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите n: ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine(SumElements(m, n));
---
+++ exercise_68/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+
+int Akkerman(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    else if (m > 0 && n == 0)
+        return Akkerman(m -1, 1);
+    return Akkerman(m - 1, Akkerman(m, n - 1));
+}
+
+
+Console.WriteLine("Введите m: ");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите n: ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine(Akkerman(m, n));
+
+
